670232fb13235641533e05ea0e4fc8594f3e33ef
Kevin Holligan <kholligan@gmail.com>
2015-04-01 20:37:17 -0600

    All territories added to territoryList

--- AxisAndAlliesGDX/core/src/axisandallies/MainGame.java
+++ AxisAndAlliesGDX/core/src/axisandallies/MainGame.java
@@ -13,7 +13,7 @@ public class MainGame extends ApplicationAdapter {
 	final static int WATER_TERRITORY_COUNT = 64;
 	//PlayerIDs
 	//Need to be updated
-	int playerID1 = 1, playerID2, = 2, playerID3 = 3, playerID4 = 4, playerID5 = 5;
+	int playerID1 = 1, playerID2 = 2, playerID3 = 3, playerID4 = 4, playerID5 = 5;
 	Territory[] territoryList = new Territory[MAX_TERRITORIES];
 
 	//List of initial territories and their default values
@@ -51,7 +51,7 @@ public class MainGame extends ApplicationAdapter {
 		territoryList[i++] = new Territory("Balkans", 1, 3, true, false, false, false);
 		territoryList[i++] = new Territory("Belorussia", 1, 2, true, false, false, false);
 		territoryList[i++] = new Territory("Ukraine S.S.R.", 1, 3, true, false, false, false);
-		territoryList[i++] = new Territory("West Russia", 1, 2, true, false, false);
+		territoryList[i++] = new Territory("West Russia", 1, 2, true, false, false, false);
 		territoryList[i++] = new Territory("Algeria", 1, 1, true, false, false, false);
 		territoryList[i++] = new Territory("Libya", 1, 1, true, false, false, false);
 		territoryList[i++] = new Territory("Sahara", 0, 0, true, true, false, false);
@@ -66,6 +66,45 @@ public class MainGame extends ApplicationAdapter {
 		territoryList[i++] = new Territory("Union of South Africa", 2, 2, true, false, false, false);
 		territoryList[i++] = new Territory("Mozambique", 0, 0, true, true, false, false);
 		territoryList[i++] = new Territory("French Madagascar", 2, 1, true, false, false, false);
+		territoryList[i++] = new Territory("Turkey", 0, 0, true, true, false, false);
+		territoryList[i++] = new Territory("Saudi Arabia", 0, 0, true, true, false, false);
+		territoryList[i++] = new Territory("Trans-Jordan", 2, 1, true, false, false, false);
+		territoryList[i++] = new Territory("India", 2, 3, true, false, false, true);
+		territoryList[i++] = new Territory("Afghanistan", 0, 0, true, true, false, false);
+		territoryList[i++] = new Territory("Himalaya", 0, 0, true, true, false, false);
+		territoryList[i++] = new Territory("Karelia S.S.R.", 0, 2, true, false, false, true);
+		territoryList[i++] = new Territory("Archangel", 0, 2, true, false, false, false);
+		territoryList[i++] = new Territory("Russia", 0, 8, true, false, true, true);
+		territoryList[i++] = new Territory("Caucasus",0, 4, true, false, false, false);
+		territoryList[i++] = new Territory("Kazakh S.S.R.", 0, 2, true, false, false, false);
+		territoryList[i++] = new Territory("Novosibirsk", 0, 2, true, false, false, false);
+		territoryList[i++] = new Territory("Evenki National Okrug", 0, 1, true, false, false, false);
+		territoryList[i++] = new Territory("Sinkiang", 4, 2, true, false, false, false);
+		territoryList[i++] = new Territory("China", 4, 2, true, false, false, false);
+		territoryList[i++] = new Territory("Yakut S.S.R.", 0, 1, true, false, false, false);
+		territoryList[i++] = new Territory("Soviet Far East", 0, 1, true, false, false, false);
+		territoryList[i++] = new Territory("Buryatia S.S.R.", 0, 1, true, false, false, false);
+		territoryList[i++] = new Territory("Manchuria", 3, 3, true, false, false, false);
+		territoryList[i++] = new Territory("Kwangtung", 3, 3, true, false, false, true);
+		territoryList[i++] = new Territory("French Indochina", 3, 3, true, false, false, false);
+		territoryList[i++] = new Territory("Japan", 3, 8, true, false, true, true);
+		territoryList[i++] = new Territory("Philippine Islands", 3, 3, true, false, false, true);
+		territoryList[i++] = new Territory("Okinawa", 3, 1, true, false, false, false);
+		territoryList[i++] = new Territory("Wake Island", 3, 0, true, false, false, false);
+		territoryList[i++] = new Territory("East Indies", 3, 4, true, false, false, false);
+		territoryList[i++] = new Territory("Borneo", 3, 4, true, false, false, false);
+		territoryList[i++] = new Territory("New Guinea", 3, 1, true, false, false, false);
+		territoryList[i++] = new Territory("Caroline Island", 3, 0, true, false, false, false);
+		territoryList[i++] = new Territory("Solomon Islands", 3, 0, true, false, false, false);
+		territoryList[i++] = new Territory("Australia", 2, 2, true, false, false, false);
+		territoryList[i++] = new Territory("New Zealand", 2, 1, true, false, false, false);
+		territoryList[i++] = new Territory("Hawaiian Islands", 4, 1, true, false, false, false);
+		territoryList[i++] = new Territory("Midway Island", 4, 0, true, false, false, false);
+		territoryList[i++] = new Territory("Alaska", 4, 2, true, false, false, false);
+		territoryList[i++] = new Territory("Western Canada", 2, 1, true, false, false, false);
+		territoryList[i++] = new Territory("Western United States", 4, 10, true, false, false, true);
+		territoryList[i++] = new Territory("Central United States", 4, 6, true, false, false, false);
+		territoryList[i++] = new Territory("Mexico", 4, 2, true, false, false, false);
 	}
 		
 
