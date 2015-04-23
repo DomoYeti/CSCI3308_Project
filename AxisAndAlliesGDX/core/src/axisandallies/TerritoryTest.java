7a814c200c5cd05ae6d6d07253d27e47092691f2
Kevin Holligan <kholligan@gmail.com>
2015-04-22 23:34:34 -0600

    Old test JUnit test file

--- AxisAndAlliesGDX/core/src/axisandallies/TerritoryTest.java
+++ AxisAndAlliesGDX/core/src/axisandallies/TerritoryTest.java
@@ -1,38 +1,42 @@
 package axisandallies;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
-import org.junit.Assert.*;
 
-public class TerritoryTest{
+public class TerritoryTest
+{
+   
+   @Test
+   public void testName(){
+      Territory testTerritory = new Territory();
 
-	@test
-	public void testName(){
-		Territory testTerritory = new Territory();
+      boolean result = testTerritory.setName("New Name");
+      assertEquals(true, result);
+   }
+   @Test
+   public void testFaction(){
+      Territory testTer = new Territory();
 
-		boolean result = testTerritory.setName("New Name");
-		assertEquals(true, result);
-	}
-	
-	public void testFaction(){
-		Territory testTer = new Territory();
+      assertEquals("Faction -1 should be false", false, testTer.setFaction(-1));
+      assertEquals("Faction 3 should be true", true, testTer.setFaction(3));
+      assertEquals("Faction 5 should be false", false, testTer.setFaction(5));
+   }
+   @Test
+   public void testIC(){
+      Territory testTer = new Territory("", 0, 0, true, false, false, false,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, false, false);
+      
+      assertEquals("Return true if adding IC when none exists", true, testTer.setIndustrialComplex(true));
+      assertEquals("Return false if adding IC when one currently exists", false, testTer.setIndustrialComplex(true));
+   }
+   @Test
+   public void testInfantry(){
+      Territory testTer = new Territory();
 
-		assertEquals("Faction -1 should be false", false, testTer.setFaction(-1));
-		assertEquals("Faction 3 should be true", true, testTer.setFaction(3));
-		assertEquals("Faction 5 should be false", false, testTer.setFaction(5));
-	}
+      assertEquals("Return false if faction less than 0", false, testTer.setInfantry(-1, 10));
+      assertEquals("Return false if number of units less than 0", false, testTer.setInfantry(0, -1));
+   }
 
-	public void testIC(){
-		Territory testTer = new Territory();
-		
-		assertEquals("Return true if adding IC when none exists", true, testTer.setFaction(true));
-		assertEquals("Return false if adding IC when one currently exists", false, testTer.setFaction(true));
-	}
-
-	public void testInfantry(){
-		Territory testTer = new Territory();
-
-		assertEquals("Return false if faction less than 0", false, testTer.setInfantry(-1, 10));
-		assertEquals("Return false if number of units less than 0", false, testTer.setInfantry(0, -1));
-	}
+   
 }
-
