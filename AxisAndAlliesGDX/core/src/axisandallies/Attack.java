5c0f049f54abddad740a8c2b34160fa6fe23dd7d
FlintTD <erha8359@colorado.edu>
2015-04-23 19:21:59 -0600

    finished Attack helper functions

--- AxisAndAlliesGDX/core/src/axisandallies/Attack.java
+++ AxisAndAlliesGDX/core/src/axisandallies/Attack.java
@@ -47,6 +47,17 @@ public class Attack {
 	setDestroyer(0,0);
     }
     
+    /**
+     * Constructor for the Attack class.
+     * Format: attacking territory, defending territory,
+     * number of the following units: attacking infantry, defending infantry,
+     * attacking artillery, defending artillery, attacking tanks,
+     * defending tanks, attacking fighters, defending fighters,
+     * attacking bombers, defending fighters, attacking battleships,
+     * defending battleships, attacking carriers, defending carriers,
+     * attacking transports, defending transports, attacking submarines,
+     * defending submarines, attacking destroyers, defending destroyers.
+     */
     public Attack(Territory attacker, Territory defender, int aInfantry,
             int dInfantry, int aArtillery, int dArtillery, int aTank, int dTank,
             int aFighter, int dFighter, int aBomber, int dBomber,
@@ -65,50 +76,90 @@ public class Attack {
 	setTransport(aTransport, dTransport);
 	setSubmarine(aSubmarine, dSubmarine);
 	setDestroyer(aDestroyer, dDestroyer);
-	setAntiAircraft(antiaircraftGun);
     }
     
     //Accessors
-    public int getInfantry()
+    public int getInfantry(String type)
     {
-        return this.aInfantry;
+        if(type == "attacker") return this.aInfantry;
+        else if (type == "a") return this.aInfantry;
+        else if(type == "defender") return this.dInfantry;
+        else if(type == "d") return this.dInfantry;
+        else return -1;
     }
-    public int getArtillery()
+    public int getArtillery(String type)
     {
-        return this.artillery;
+        if(type == "attacker") return this.aArtillery;
+        else if (type == "a") return this.aArtillery;
+        else if(type == "defender") return this.dArtillery;
+        else if(type == "d") return this.dArtillery;
+        else return -1;
     }
-    public int getTank()
+    public int getTank(String type)
     {
-        return this.tank;
+        if(type == "attacker") return this.aTank;
+        else if (type == "a") return this.aTank;
+        else if(type == "defender") return this.dTank;
+        else if(type == "d") return this.dTank;
+        else return -1;
     }
-    public int getFight()
+    public int getFighter(String type)
     {
-        return this.fighter;
+        if(type == "attacker") return this.aFighter;
+        else if (type == "a") return this.aFighter;
+        else if(type == "defender") return this.dFighter;
+        else if(type == "d") return this.dFighter;
+        else return -1;
     }
-    public int getBomber()
+    public int getBomber(String type)
     {
-        return this.bomber;
+        if(type == "attacker") return this.aBomber;
+        else if (type == "a") return this.aBomber;
+        else if(type == "defender") return this.dBomber;
+        else if(type == "d") return this.dBomber;
+        else return -1;
     }
-    public int getBattleship()
+    public int getBattleship(String type)
     {
-        return this.battleship;
+        if(type == "attacker") return this.aBattleship;
+        else if (type == "a") return this.aBattleship;
+        else if(type == "defender") return this.dBattleship;
+        else if(type == "d") return this.dBattleship;
+        else return -1;
     }
-    public int getAircraftCarrier()
+    public int getAircraftCarrier(String type)
     {
-        return this.aircraftCarrier;
+        if(type == "attacker") return this.aAircraftCarrier;
+        else if (type == "a") return this.aAircraftCarrier;
+        else if(type == "defender") return this.dAircraftCarrier;
+        else if(type == "d") return this.dAircraftCarrier;
+        else return -1;
     }
-    public int getTransport()
+    public int getTransport(String type)
     {
-        return this.transport;
+        if(type == "attacker") return this.aTransport;
+        else if (type == "a") return this.aTransport;
+        else if(type == "defender") return this.dTransport;
+        else if(type == "d") return this.dTransport;
+        else return -1;
     }
-    public int getSubmarine()
+    public int getSubmarine(String type)
     {
-        return this.submarine;
+        if(type == "attacker") return this.aSubmarine;
+        else if (type == "a") return this.aSubmarine;
+        else if(type == "defender") return this.dSubmarine;
+        else if(type == "d") return this.dSubmarine;
+        else return -1;
     }
-    public int getDestroyer()
+    public int getDestroyer(String type)
     {
-        return this.destroyer;
+        if(type == "attacker") return this.aDestroyer;
+        else if (type == "a") return this.aDestroyer;
+        else if(type == "defender") return this.dDestroyer;
+        else if(type == "d") return this.dDestroyer;
+        else return -1;
     }
+    
     
     //Mutators
    public void setInfantry(int attackers, int defenders)
